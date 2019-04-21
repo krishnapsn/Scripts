4546a64ac918fea0e4739ebd10183615bfe27a39
psycholoony <krishnapsnambiar@gmail.com>
2019-04-21 15:33:04 +0530

    added photo download utility

--- git CLI/git.sh
+++ git CLI/git.sh
@@ -1,13 +1,35 @@
-cls && cat 2.txt
+#!/usr/bin/bash
+clear && cat 2.txt
+
 read usern
-if (python git.py $usern) then
+
+python git.py $usern
+
+if [ $? -ne 0 ] 
+then
+	echo "ERROR"
+else
+	setopt shwordsplit
+	for i in $(python git2.py $usern) 
+	do
+		echo "id :: $i"
+		curl -o $i.jpg https://avatars0.githubusercontent.com/u/"$i"
+	done
+	echo "RANDOM DAD JOKE FOR YOU XD"
+	curl -H "Accept: text/plain" https://icanhazdadjoke.com/
+	echo "\n"
 	cat 1.txt
 	read repo
-	if ! (git clone https://github.com/$usern/$repo.git) then
-		echo "FAIL"
+
+	echo "ENTER DIRECTORY TO CLONE INTO"
+	tree -d ~ -L 1
+	read dir
+	cd $dir
+	git clone https://github.com/$usern/$repo.git
+	if  [ $? -ne 0 ] 
+	then
+	    echo "ERROR"
 	else
-	    echo "REPO CLONED"
+		echo "Cloned"
 	fi
-else
-	echo "ERROR"
 fi
